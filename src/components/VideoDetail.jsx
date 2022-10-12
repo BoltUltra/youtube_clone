6e98e6914b8a753113996f9673db5ff6fb00b4f0
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-10-12 18:51:23 +0100

    ...

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { CheckCircle } from '@mui/icons-material';
+import { Box, Stack, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import { Link, useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
+import { Video } from './';
 const VideoDetail = () => {
+	const { id } = useParams();
+	useEffect(()=>{
+fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+.then((data)=>)
+	}, [id])
 	return (
-		<div>
-			<p>VideoDetail</p>
-		</div>
+		<Box minHeight="95vh">
+			<Stack direction={{ xs: 'column', md: 'row' }}>
+				<Box flex={1}>
+					<Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+						<ReactPlayer />
+					</Box>
+				</Box>
+			</Stack>
+		</Box>
 	);
 };
 
