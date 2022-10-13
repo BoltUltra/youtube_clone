a628487b739872a240855adf8bf66a4005ddd742
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-10-13 08:29:58 +0100

    updates

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,28 +1,37 @@
-import { CheckCircle } from '@mui/icons-material';
-import { Box, Stack, Typography } from '@mui/material';
-import { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player';
-import { Link, useParams } from 'react-router-dom';
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { CheckCircle } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import { Link, useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-import { Video } from './';
+import { Video } from "./";
 const VideoDetail = () => {
-	const { id } = useParams();
-	useEffect(()=>{
-fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-.then((data)=>)
-	}, [id])
-	return (
-		<Box minHeight="95vh">
-			<Stack direction={{ xs: 'column', md: 'row' }}>
-				<Box flex={1}>
-					<Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-						<ReactPlayer />
-					</Box>
-				</Box>
-			</Stack>
-		</Box>
-	);
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {videoDetail.snippet.title}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
